063e9116ea5eae5541bcffd822bb1d5e8e18dfe2
hj-k66 <hjx66@naver.com>
2024-10-30 10:54:29 +0900

    [INIT] 백엔드 프로젝트 초기 설정

---
+++ aingle-back/src/main/java/com/aintopia/aingle/AingleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aintopia.aingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AingleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AingleBackendApplication.class, args);
+    }
+
+}
