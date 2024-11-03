2fc4af6a1c012ae66d18c5a47eb29fede03e2c7f
gitlab 이름 <nwy98@naver.com>
2024-11-04 02:23:54 +0900

    [FEAT] 리액트 쿼리, 리코일 설정

---
+++ aingle-front/src/store/atoms.ts
@@ -0,0 +1,40 @@
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "recoil-persist", // 이 키로 localStorage에 저장됩니다
+  storage: localStorage, // localStorage를 사용하거나 sessionStorage로 변경 가능
+});
+
+export const userDataState = atom({
+  key: "userDataState", // 고유한 키
+  default: {
+    nickName: "",
+    exp: 0,
+    profile: "",
+  },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const userIdState = atom({
+  key: "userIdState",
+  default: {
+    id: 0,
+  },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const geolocationState = atom({
+  key: "geolocationState",
+  default: {
+    longitude: 0,
+    latitude: 0,
+  },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const locationState = atom({
+  key: "locationState",
+  default: "",
+  effects_UNSTABLE: [persistAtom],
+});
