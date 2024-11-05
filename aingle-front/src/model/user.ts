88e0b29c253266e0e61cca26cac202de63a7082f
김준혁 <junhuk4079@gmail.com>
2024-11-05 09:17:50 +0900

    [FEAT] signUpRequestDto 인터페이스 추가

--- aingle-front/src/model/user.ts
+++ aingle-front/src/model/user.ts
@@ -1,3 +1,8 @@
-interface IUser {
+export interface ImemberSignUpRequestDto {
   name: string;
+  email: string;
+  birth: string;
+  platform: "kakao" | "google";
+  language: "korean" | "english";
+  file: File;
 }
