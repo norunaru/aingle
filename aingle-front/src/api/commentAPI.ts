501ad32477876a3770b048741fb6c991bd0b88fd
노우영 <nwy98@naver.com>
2024-11-06 16:42:55 +0900

    [FEAT] GA 세팅, 대댓글 api

--- aingle-front/src/api/commentAPI.ts
+++ aingle-front/src/api/commentAPI.ts
@@ -36,3 +36,37 @@ export const deleteComments = async (commentId: number) => {
     console.error("게시글 댓글 조회에 실패 : ", error);
   }
 };
+
+// 대댓글 등록
+export const createReply = async (commentId: number, content: string) => {
+  try {
+    // formData 인수로 담아서 요청
+    const response = await axiosInstance.post(`${BASE_URL}/replies`, {
+      commentId,
+      content,
+    });
+
+    console.log(`댓글 ${commentId}에 대댓글 달기 성공 : `, response);
+
+    return response.data;
+  } catch (error) {
+    console.error("댓글 등록 실패 : ", error);
+    throw error;
+  }
+};
+
+// 대댓글 삭제
+export const deleteReply = async (replyId: number) => {
+  try {
+    const response = await axiosInstance.delete(
+      `${BASE_URL}/replies/${replyId}`
+    );
+
+    console.log("대댓글 삭제 성공 ");
+
+    return response;
+  } catch (error) {
+    console.error("대댓글 삭제 실패 : ", error);
+    throw error;
+  }
+};
