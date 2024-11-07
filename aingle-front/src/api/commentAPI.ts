c415432caefba9ede2a6865e65100929e2d17556
김준혁 <junhuk4079@gmail.com>
2024-11-07 10:09:38 +0900

    [CHORE] 주석 제거 임포트문 철자 변경

--- aingle-front/src/api/commentAPI.ts
+++ aingle-front/src/api/commentAPI.ts
@@ -1,11 +1,11 @@
 import { IcreateComment } from "../model/comment";
 import { BASE_URL } from "./APIconfig";
-import axiosInstance from "./axiosinstance";
+import axiosInstance from "./axiosInstance";
 
 // 댓글 등록 api (완료)
 export const createComment = async (comment: IcreateComment) => {
   try {
-    // formData 인수로 담아서 요청
+    
     const response = await axiosInstance.post(`${BASE_URL}/comments`, comment);
 
     return response.data;
