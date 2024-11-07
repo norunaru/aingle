2bcedfaff8c82473a2f03621bd03c786a9e68ec2
hj-k66 <hjx66@naver.com>
2024-11-07 10:27:02 +0900

    [FIX] 투표 에러 상태코드 409로 설정

---
+++ aingle-back/src/main/java/com/aintopia/aingle/common/util/ExceptionManager.java
@@ -0,0 +1,21 @@
+package com.aintopia.aingle.common.util;
+
+import com.aintopia.aingle.vote.exception.BadRequestCharacterException;
+import com.aintopia.aingle.vote.exception.BadRequestVoteException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionManager {
+    @ExceptionHandler(BadRequestVoteException.class)
+    public ResponseEntity<String> handleBadRequestPostException(BadRequestVoteException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(BadRequestCharacterException.class)
+    public ResponseEntity<String> handleBadRequestPostException(BadRequestCharacterException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+}
