a0e50f1af068831589963630a571695c59ffbf9d
김준혁 <junhuk4079@gmail.com>
2024-11-07 13:15:43 +0900

    [FEAT] 투표 알람 여부 체크이후 출력문 다르게 설정

--- aingle-front/src/components/card/NoticeCard.tsx
+++ aingle-front/src/components/card/NoticeCard.tsx
@@ -1,39 +1,38 @@
 import React from "react";
 import { Ialarm } from "../../model/alarm";
 import { calTime } from "../../utils/date";
-interface alarmProps {
-  alarm : Ialarm
+
+interface AlarmProps {
+  alarm: Ialarm;
+  onClickAlarm: () => void;
 }
 
-const NoticeCard = ( {alarm} : alarmProps) => {
+const NoticeCard: React.FC<AlarmProps> = ({ alarm, onClickAlarm }) => {
+  const { sender, post, vote } = alarm;
 
-  const { sender , post } = alarm;
+  // 시간을 계산하는 함수 호출
+  const time = calTime(alarm.createTime);
 
-  const time = calTime(alarm.createTime); 
-
-  // const cutText = (text: string): string => {
-  //   if (text.length > 14) {
-  //     return text.substring(0, 14) + "...";
-  //   }
-  //   return text;
-  // };
+  // 알림 텍스트 설정
+  const alarmText = vote
+    ? "이번달 투표 결과가 나왔습니다"
+    : "회원님의 글에 반응을 남겼습니다";
 
   return (
     <div
-      className={`mb-[10px] w-full p-[15px] rounded-[10px] flex gap-[12px] items-start bg-[#FFE8F1] ${
+      className={`mb-[10px] w-full p-[15px] rounded-[10px] flex gap-[12px] items-start ${
         alarm.isRead ? "bg-[rgb(241,241,241)]" : "bg-[#FFE8F1]"
       }`}
+      onClick={onClickAlarm}
     >
       <img
-        src={sender?.memberImage}
+        src={sender?.memberImage || ""}
+        alt="프로필 이미지"
         className="w-[30px] h-[30px] rounded-full bg-black object-cover flex-shrink-0"
       />
       <div className="w-full">
         <h1 className="text-16px font-semibold mb-[5px]">{sender?.name}</h1>
-        <h1 className="text-[14px]">{}</h1>
-        <h1 className="text-[14px] text-pink-base mb-[2px]">
-          {/* {cutText(content)} */}
-        </h1>
+        <h1 className="text-[14px]">{alarmText}</h1>
         <span className="text-[#A6A6A6] text-[12px]">{time}</span>
       </div>
       {post.image && (
