5cf292c72556589795346a1d1b53db71164aa9fe
김준혁 <junhuk4079@gmail.com>
2024-11-07 10:10:35 +0900

    [CHORE] 좋아요 API 요청 수정 // postId를 객체 형태로 요청

--- aingle-front/src/api/likeAPI.ts
+++ aingle-front/src/api/likeAPI.ts
@@ -1,15 +1,16 @@
 import { BASE_URL } from "./APIconfig";
-import axiosInstance from "./axiosinstance";
+import axiosInstance from "./axiosInstance";
 
 // 좋아요 등록시 사용하는 API
 export const like = async(postId : number) => {
     try {
-        const response = await axiosInstance.post(`${BASE_URL}/likes` , postId)
+      // 여기 {} 객체 형태로 묶어서 보내야 서버에서 처리 가능
+        const response = await axiosInstance.post(`${BASE_URL}/likes` , { postId })
         return response.data;
 
     } catch (error) {
         
-        console.error("좋아요 실패 : " , error);
+        console.error("좋아요 등록 실패 : " , error);
      
     }
 }
@@ -17,9 +18,9 @@ export const like = async(postId : number) => {
 // 좋아요 삭제시 사용하는 API
 export const disLike = async (postId: number) => {
   try {
-    const response = await axiosInstance.patch(`${BASE_URL}/likes`, postId);
+    const response = await axiosInstance.patch(`${BASE_URL}/likes`, { postId });
     return response.data;
   } catch (error) {
-    console.error("좋아요 실패 : ", error);
+    console.error("좋아요 삭제 실패 : ", error);
   }
 };
