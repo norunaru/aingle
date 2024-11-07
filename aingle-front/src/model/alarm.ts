72ed4a57ee19875ef0cbff6fbe73f1cb8768e796
김준혁 <junhuk4079@gmail.com>
2024-11-07 11:16:28 +0900

    [CHORE] 알람 내부에 있는 sender post 인터페이스 추가

--- aingle-front/src/model/alarm.ts
+++ aingle-front/src/model/alarm.ts
@@ -1,11 +1,25 @@
+// alarm에 포함되는 알림 sender 정보
+export interface Isender {
+    memberId : number;
+    name : string;
+    memberImage : string;
+}
+
+// alarm에 포함되는 게시글 정보
+export interface Ipost {
+    postId : number;
+    image : string;
+}
+
 export interface Ialarm {
     alarmId : number;
     isRead : boolean;
-    post : {
-        postId : number;
-        image : string;
-    }
+    post : Ipost;
+    // 투표 알림이 아닐 경우 null이 반환되기 때문에 null 처리
     vote : number | null;
+    createTime : string;
+    // vote 타입의 알림인 경우 sender가 없기 때문에 null 처리
+    sender : Isender | null;
 }
 
 export interface IgetAlarm {
