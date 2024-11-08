cfbc8cfdc88762e38f5433887bf0ee5a09317f65
노우영 <nwy98@naver.com>
2024-11-08 12:34:05 +0900

    [FEAT] GA

--- aingle-front/src/main.tsx
+++ aingle-front/src/main.tsx
@@ -1,19 +1,34 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from "recoil";
 import ReactGA from "react-ga4";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 
 ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
 
+const Analytics = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+};
+
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <RecoilRoot>
       <BrowserRouter>
+        <Analytics />
         <App />
       </BrowserRouter>
     </RecoilRoot>
