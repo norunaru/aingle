99f999178a9b4a5179c6d0ac226d04e6b110a2c6
김준혁 <junhuk4079@gmail.com>
2024-11-08 14:21:18 +0900

    [CHORE] mypage로 라우팅하고 replace 속성 추가

--- aingle-front/src/pages/vote/CharDetail.tsx
+++ aingle-front/src/pages/vote/CharDetail.tsx
@@ -38,7 +38,7 @@ const CharDetail = () => {
           icon={faChevronLeft}
           className=" text-white absolute top-[16px] left-[18px] cursor-pointer"
           onClick={() => {
-            navigate("/mypage");
+            navigate("/mypage" , {replace : true});
           }}
         />
       </div>
