86e2454c41f9b89977d00a83b049b320ba0f1619
조용훈 <sjsin0905@naver.com>
2024-11-11 16:36:13 +0900

    [FIX] 프롬프트 변경

--- aingle-back/src/main/java/com/aintopia/aingle/common/openai/OpenAIClient.java
+++ aingle-back/src/main/java/com/aintopia/aingle/common/openai/OpenAIClient.java
@@ -216,35 +216,35 @@ public class OpenAIClient {
         }
     }
 
-    //    // 게시글 생성 함수
-//    public String createImage() throws MalformedURLException {
-//        String prompt = "당신은 25세 여자 대학생 유보은입니다. 유보은이 되어서 사용자와 대화를 진행합니다. 아래의 대화 패턴을 참고해서 질문에 대해 답변해주세요. \n" +
-//            "\n" +
-//            "###대화 패턴###\n" +
-//            "- 답변은 반말로 한다.\n" +
-//            "- 답변은 간결하게 한 문장으로 한다.\n" +
-//            "- 감정표현이 풍부하다.\n" +
-//            "- 이모티콘을 사용한다. 예시) \uD83D\uDE06\uD83D\uDE2D\n" +
-//            "- 성격은 단순활발하다.\n" +
-//            "- 맛집에 관심이 많다.\n" +
-//            "- “ㅋㅋㅋ”와 같은 웃음 표현을 자주 사용한다. 유보은이 sns에 작성할 이미지를 생성해줘.";
-//
-//        ImageResponse imageResponse =  imageModel.call(
-//            new ImagePrompt(prompt,
-//                OpenAiImageOptions.builder()
-//                    .withQuality("hd")
-//                    .withStyle("vivid")
-//                    .withHeight(1024)
-//                    .withWidth(1024).build())
-//
-//        );
-//        String url = imageResponse.getResult().getOutput().getUrl();
-//        log.info("image URL : " + url);
-//        URL imageUrl = URI.create(url).toURL();
-//
-//        Message userMessage = new UserMessage("이 사진의 시드 넘버를 알려줘.", new Media(MimeTypeUtils.IMAGE_PNG, imageUrl));
-//        ChatResponse chatResponse = chatModel.call(new Prompt(userMessage, OpenAiChatOptions.builder().withModel(OpenAiApi.ChatModel.GPT_4_O.getValue()).build()));
-//        return chatResponse.getResult().getOutput().getContent();
-//    }
+        // 게시글 생성 함수
+    public String createImage() throws MalformedURLException {
+        String prompt = "당신은 25세 여자 대학생 유보은입니다. 유보은이 되어서 사용자와 대화를 진행합니다. 아래의 대화 패턴을 참고해서 질문에 대해 답변해주세요. \n" +
+            "\n" +
+            "###대화 패턴###\n" +
+            "- 답변은 반말로 한다.\n" +
+            "- 답변은 간결하게 한 문장으로 한다.\n" +
+            "- 감정표현이 풍부하다.\n" +
+            "- 이모티콘을 사용한다. 예시) \uD83D\uDE06\uD83D\uDE2D\n" +
+            "- 성격은 단순활발하다.\n" +
+            "- 맛집에 관심이 많다.\n" +
+            "- “ㅋㅋㅋ”와 같은 웃음 표현을 자주 사용한다. 유보은이 sns에 작성할 이미지를 생성해줘.";
+
+        ImageResponse imageResponse =  imageModel.call(
+            new ImagePrompt(prompt,
+                OpenAiImageOptions.builder()
+                    .withQuality("hd")
+                    .withStyle("vivid")
+                    .withHeight(1024)
+                    .withWidth(1024).build())
+
+        );
+        String url = imageResponse.getResult().getOutput().getUrl();
+        log.info("image URL : " + url);
+        URL imageUrl = URI.create(url).toURL();
+
+        Message userMessage = new UserMessage("이 사진의 시드 넘버를 알려줘.", new Media(MimeTypeUtils.IMAGE_PNG, imageUrl));
+        ChatResponse chatResponse = chatModel.call(new Prompt(userMessage, OpenAiChatOptions.builder().withModel(OpenAiApi.ChatModel.GPT_4_O.getValue()).build()));
+        return chatResponse.getResult().getOutput().getContent();
+    }
 
 }
