b68ce541a2c6e038e867b74a8525bd4b85b86fef
조용훈 <sjsin0905@naver.com>
2024-11-11 14:04:53 +0900

    [FIX] 프롬프트 변경

--- aingle-back/src/main/java/com/aintopia/aingle/common/openai/model/OpenAIPrompt.java
+++ aingle-back/src/main/java/com/aintopia/aingle/common/openai/model/OpenAIPrompt.java
@@ -15,10 +15,12 @@ public enum OpenAIPrompt {
         "- 답변은 %s로 한다.\n" +
         "- 답변은 %s으로 한다.\n" +
         "%s"),
-    AI_CHARACTER_COMMENT_REQUEST_PROMPT("사용자가 올린 사진과 내용이 담긴 sns게시글을 보고 적절한 반응을 해줘. 내용 : "),
+    AI_CHARACTER_COMMENT_REQUEST_PROMPT("사용자가 올린 사진과 내용이 담긴 sns게시글을 보고 적절한 반응을 해줘. 만약 사진을 인식 할 수 없다면 글을 보고 내용을 최대한 유추해서 반응을 해줘. 알아볼 수 없거나 모르겠다는 반응은 하지마. 내용 : "),
     AI_CHARACTER_CREATE_SUMMARY_PROMPT("%s이 자주하는 말이나 %s을 표현할 수 있는 대표적인 말을 15자 이내로 해줘."),
-    AI_CHARACTER_CREATE_POST_TEXT_PROMPT("%s(이)가 사진과 함께 SNS에 작성할 만한 글을 작성해줘 해당 인물의 성향을 잘 적용해서 짧아도 되지만 최대 200자를 넘어서는 안돼"),
-    AI_CHARACTER_CREATE_POST_PROMPT("%s이(가) SNS에 [ %s ]라고 글을 작성했어 이 글에 어울리는 실제 같은 이미지를 생성해줘");
+    AI_CHARACTER_CREATE_POST_TEXT_PROMPT("%s(이)가 사진과 함께 SNS에 작성할 만한 글을 작성해줘 해당 인물의 성향을 잘 적용해서 짧아도 되지만 최대 200자를 넘어서는 안돼 다른 답변은 하지마"),
+    AI_CHARACTER_CREATE_POST_PROMPT("%s이(가) SNS에 [ %s ]라고 글을 작성했어 이 글에 어울리는 실제 같은 이미지를 조건에 충실하게 생성해줘\n"
+        + "조건 1. 인물 및 캐릭터를 이미지에 넣지 말것\n"
+        + "조건 2. LLM 모델이 인식 하기 쉬운 이미지로 생성할 것\n");
 
     private final String promptTemplate;
 
