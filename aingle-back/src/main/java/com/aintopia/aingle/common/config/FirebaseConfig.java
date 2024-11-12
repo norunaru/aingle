d9e7bb19eabe8f5693b5e752f6888f82ec828c7c
SeongJeong <jsj046@naver.com>
2024-11-12 17:48:24 +0900

    [HOTFIX] 경로 수정

--- aingle-back/src/main/java/com/aintopia/aingle/common/config/FirebaseConfig.java
+++ aingle-back/src/main/java/com/aintopia/aingle/common/config/FirebaseConfig.java
@@ -6,27 +6,36 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    private final ResourceLoader resourceLoader;
+
     @Value("${firebase.config-path}")
     private String firebaseConfigPath;
 
+    public FirebaseConfig(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
+        Resource resource = resourceLoader.getResource(firebaseConfigPath);
+        try (InputStream serviceAccount = resource.getInputStream()) {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
-
-        if (FirebaseApp.getApps().isEmpty()) { // Firebase가 초기화되지 않은 경우
-            FirebaseApp.initializeApp(options);
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+            return FirebaseApp.getInstance();
         }
-        return FirebaseApp.getInstance();
     }
 }
