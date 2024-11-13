21f305901cdd65ee56601a42940e3ee9e4e32190
노우영 <nwy98@naver.com>
2024-11-13 14:24:26 +0900

    [FIX] fb6

--- aingle-front/public/firebase-messaging-sw.js
+++ aingle-front/public/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts("https://www.gstatic.com/firebasejs/9.1.1/firebase-messaging.js");
 
 // Firebase 초기화
 firebase.initializeApp({
-  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
-  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_REACT_APP_APP_ID,
-  measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
+  apiKey: process.env.VITE_REACT_APP_API_KEY,
+  authDomain: process.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: process.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_REACT_APP_APP_ID,
+  measurementId: process.env.VITE_REACT_APP_MEASUREMENT_ID,
 });
 
 const messaging = firebase.messaging();
--- aingle-front/vite.config.ts
+++ aingle-front/vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import replace from "@rollup/plugin-replace";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-  },
+  plugins: [
+    replace({
+      preventAssignment: true,
+      "process.env.VITE_REACT_APP_API_KEY": JSON.stringify(
+        process.env.VITE_REACT_APP_API_KEY
+      ),
+      "process.env.VITE_REACT_APP_AUTH_DOMAIN": JSON.stringify(
+        process.env.VITE_REACT_APP_AUTH_DOMAIN
+      ),
+      "process.env.VITE_REACT_APP_PROJECT_ID": JSON.stringify(
+        process.env.VITE_REACT_APP_PROJECT_ID
+      ),
+      "process.env.VITE_REACT_APP_STORAGE_BUCKET": JSON.stringify(
+        process.env.VITE_REACT_APP_STORAGE_BUCKET
+      ),
+      "process.env.VITE_REACT_APP_MESSAGING_SENDER_ID": JSON.stringify(
+        process.env.VITE_REACT_APP_MESSAGING_SENDER_ID
+      ),
+      "process.env.VITE_REACT_APP_APP_ID": JSON.stringify(
+        process.env.VITE_REACT_APP_APP_ID
+      ),
+      "process.env.VITE_REACT_APP_MEASUREMENT_ID": JSON.stringify(
+        process.env.VITE_REACT_APP_MEASUREMENT_ID
+      ),
+    }),
+  ],
 });
