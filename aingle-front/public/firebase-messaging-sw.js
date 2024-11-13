b5c71e2022d177741e16bd5183bf8d09470c1b5d
노우영 <nwy98@naver.com>
2024-11-13 14:17:57 +0900

    [FIX] fb5

--- aingle-front/public/firebase-messaging-sw.js
+++ aingle-front/public/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts("https://www.gstatic.com/firebasejs/9.1.1/firebase-messaging.js");
 
 // Firebase 초기화
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID",
+  apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID,
+  measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
 });
 
 const messaging = firebase.messaging();
