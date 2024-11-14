76f3af6cae258d57ef1848678c47f5787832f798
SeongJeong <jsj046@naver.com>
2024-11-14 10:47:15 +0900

    [FEAT] 대댓글 알림 생성

--- aingle-back/src/main/java/com/aintopia/aingle/common/service/FcmService.java
+++ aingle-back/src/main/java/com/aintopia/aingle/common/service/FcmService.java
@@ -7,7 +7,6 @@ import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionalEventListener;
 
